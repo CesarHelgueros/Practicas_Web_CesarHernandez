9b4676e7afa37eb51e3912a9669619f52bb05e1c
Hernandez Helgueros Cesar Alexis <helgueroscesar@micorreo.upp.edu.mx>
2026-02-20 10:03:34 -0600

    EXTRA P9

--- practica 9/P9/src/app/components/formulario/formulario.ts
+++ practica 9/P9/src/app/components/formulario/formulario.ts
@@ -1,39 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Alumnos } from '../../services/alumnos';
-
+declare var bootstrap:any;
 @Component({
   selector: 'app-formulario',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './formulario.html'
+  templateUrl: './formulario.html',
+  styleUrl: './formulario.css',
 })
 export class Formulario {
-
-  constructor(
-    private alumnoService: Alumnos,
-    private router: Router
-  ) {}
-
-  guardarAlumno(form: any) {
-
+  mostrarModal: boolean = false;
+  constructor(private alumno:Alumnos, private router: Router){}
+  guardarAlumno(form:NgForm){
+    if (form.invalid) return;
     const nuevoAlumno = {
       nombre: form.value.nombre,
       edad: form.value.edad,
       carrera: form.value.carrera
-    };
-
-    this.alumnoService.agregarAlumno(nuevoAlumno).subscribe({
-      next: () => {
-        alert("Alumno agregado correctamente");
-        form.reset();
-        this.router.navigate(['/']);  // ← ESTA ES LA LÍNEA IMPORTANTE
-      },
-      error: (err) => {
-        console.error("Error:", err);
-      }
-    });
+};
+  this.alumno.agregarAlumno(nuevoAlumno).subscribe({
+    next: () => {
+    this.mostrarModal = true;
+    
+    form.reset();
+  },
+  error: (err) => {
+    console.error("Error al insertar:", err);
   }
+});
+  }
+  cerrarModal() {
+    this.mostrarModal = false;
+    this.router.navigate(['/consulta']);
+}
 }
